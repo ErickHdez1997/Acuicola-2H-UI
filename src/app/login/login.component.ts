4ab94538e7c23c5d97a2e32c68bafaf6b8933749
Erick Hernandez <Erick.Hdez199721@gmail.com>
2024-06-27 18:29:59 -0400

    WIP

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, 
+    ReactiveFormsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -49,5 +59,9 @@ export class LoginComponent {
 
   navigateToRegister(){
     this.router.navigate(['/register']);
+  }
+
+  updateErrorMessage() {
+    if (this.loginForm.get)
   }
 }
